972855d7c81b24d75731ee890969c846c617905f
Wanda Hawkins-Kavanagh <wandahawkinskavanagh@Wandas-MacBook-Pro.local>
2026-08-20 16:02:52 +0100

    added new test

---
+++ tests/invalidtest.test.js
@@ -0,0 +1,53 @@
+const { spec } = require('pactum');
+const bookingSchema = require('../booking.schema.json');
+
+describe('booking and updating a booking', () => {
+    let bookingId;
+
+    it('verify creating a successful booking', async () => {
+        let url = 'https://restful-booker.herokuapp.com/booking';
+
+        const response = await spec()
+            .post(url)
+            .withHeaders({
+                'content-type': 'application/json',
+                'Accept': 'application/json'
+            })
+            .withJson({
+                "firstname": "Wanda",
+                "lastname": "Brown",
+                "totalprice": 567,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2020-01-01",
+                    "checkout": "2021-01-01"
+                },
+                "additionalneeds": "Breakfast, Lunch"
+            })
+            .expectStatus(200);
+        bookingId = response.body.bookingid;
+    })
+    it('should generate a 400 response for an invalid request', async () => {
+        let url = `https://restful-booker.herokuapp.com/booking/${bookingId}`;
+
+        await spec()
+            .put(url)
+            .withHeaders({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='
+            })
+            .withJson({
+                "lastname": "Brown",
+                "totalprice": 567,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2020-01-01",
+                    "checkout": "2021-01-01"
+                },
+                "additionalneeds": "Breakfast, Lunch"
+            })
+            .expectStatus(400)
+            .expectBody('Bad Request');
+});
+});
