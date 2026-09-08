71b958f33fcb86b1d848f2c06cb75eb0937cfcc1
Wanda Hawkins-Kavanagh <wandahawkinskavanagh@Wandas-MacBook-Pro.local>
2026-09-08 14:40:58 +0100

    added helper for booking

--- tests/invalidToken.test.js
+++ tests/invalidToken.test.js
@@ -1,31 +1,12 @@
 const { spec } = require('pactum');
 const bookingSchema = require('../booking.schema.json');
+const { createBookingRequest } = require('../helpers/bookingSetup');
 
 describe('booking and updating a booking', () => {
     let bookingId;
 
     it('verify creating a successful booking', async () => {
-        let url = 'https://restful-booker.herokuapp.com/booking';
-
-        const response = await spec()
-            .post(url)
-            .withHeaders({
-                'content-type': 'application/json',
-                'Accept': 'application/json'
-            })
-            .withJson({
-                "firstname": "Wanda",
-                "lastname": "Brown",
-                "totalprice": 567,
-                "depositpaid": true,
-                "bookingdates": {
-                    "checkin": "2020-01-01",
-                    "checkout": "2021-01-01"
-                },
-                "additionalneeds": "Breakfast, Lunch"
-            })
-            .expectStatus(200);
-        bookingId = response.body.bookingid;
+        bookingId = await createBookingRequest();
     })
     it('should generate a 400 response when the token is invalid', async () => {
         let url = `https://restful-booker.herokuapp.com/booking/${bookingId}`;
